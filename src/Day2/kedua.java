7021911cc35a3d9bc9c89eb776fdb09f14f0eeed
pauldaviddjukardi@gmail.com <pauldaviddjukardi.com>
2025-05-14 22:30:47 +0700

    fix day 2

--- src/Day2/kedua.java
+++ src/Day2/kedua.java
@@ -2,7 +2,27 @@ package src.Day2;
 
 public class kedua {
     // Array 
-    String arrayString;
-    arrayString = new String[4];
-    arrayString[0] = "nol"
+   public static void main(String[] args) {
+    String[] arrayStrin;
+    arrayStrin = new String[4];
+
+    arrayStrin[0] = "nol";
+    arrayStrin[1] = "satu";
+    arrayStrin[2] = "dua";
+    arrayStrin[3] = "tiga";
+
+    System.out.println(arrayStrin[1]);
+
+    int[] arrayInt = new int[]{
+        1, 2, 4, 5, 3, 6
+    };
+
+    long[] arrayLong = {
+        1, 2, 3, 4, 5, 6, 7
+    };
+   }
+
+   //Operasi array
+
+   
 }
