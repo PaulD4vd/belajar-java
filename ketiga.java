b6dee73bbd992f861de676d26effbf3c6b327215
PaulD4vd <pauldaviddjukardi@gmail.com>
2025-05-21 05:59:06 -0700

    Day 3

---
+++ ketiga.java
@@ -0,0 +1,17 @@
+
+
+public class ketiga {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        long[] arrayLong = { 
+            10L, 20L, 30L
+    };
+        int[] variabeInt = new int[] {
+             1, 2, 3, 4, 5, 6
+    };
+        System.out.println(variabeInt[0]);
+        System.out.println(variabeInt[1]);
+           
+    };
+}
